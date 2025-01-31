56a911c9002e8d11a74e9a797c55a1efdad14039
Tarun 2 <141573811+tarunmandhan2@users.noreply.github.com>
2025-01-31 10:44:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>16. Implement a Context API for Global State</h1>
+      <h4>
+        Create a global state using React's Context API to manage the state of
+        posts across the application.
+      </h4>
+    </div>
+  );
+}
